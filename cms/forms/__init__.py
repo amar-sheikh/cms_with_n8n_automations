4e6549cca72ed77d0af03302405353435c7fd2b6
umar <amar.m.sheikh@gmail.com>
2025-07-07 12:46:30 +0500

    Implement file upload to n8n webhook trigger

--- cms/forms/__init__.py
+++ cms/forms/__init__.py
@@ -1,5 +1,6 @@
 from .batch_form import BatchForm, BatchSlotCreateFormSet, BatchSlotUpdateFormSet
 from .course_form import CourseForm
+from .image_form import ImageForm
 from .instructor_form import InstructorForm, InstructorSlotCreateFormSet, InstructorSlotUpdateFormSet
 from .parent_form import ParentForm
 from .student_form import StudentForm
--- cms/views/course_view.py
+++ cms/views/course_view.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from cms.forms import CourseForm
+from cms.forms import CourseForm, ImageForm
 from cms.models import Course
 
 class CourseListView(ListView):
@@ -13,14 +13,70 @@ class CourseListView(ListView):
 class CourseCreateView(CreateView):
     model = Course
     template_name = 'courses/_form.html'
-    form_class=CourseForm
     success_url = reverse_lazy('courses')
+
+    def get(self, request, *args, **kwargs):
+        course_form = CourseForm()
+        image_form = ImageForm()
+        return render(request, self.template_name, {
+            'form': course_form,
+            'image_form': image_form
+        })
+
+    def post(self, request, *args, **kwargs):
+        course_form = CourseForm(request.POST)
+        image_form = ImageForm(request.POST, request.FILES)
+        if course_form.is_valid() and image_form.is_valid():
+            course = course_form.save(commit=False)
+            image = image_form.save()
+            course.image = image
+            course.save()
+            return redirect('courses')
+
+        return render(request, self.template_name, {
+            'form': course_form,
+            'image_form': image_form
+        })
 
 class CourseUpdateView(UpdateView):
     model = Course
     template_name = 'courses/_form.html'
     form_class=CourseForm
     success_url = reverse_lazy('courses')
+
+    def get(self, request, *args, **kwargs):
+        course = self.get_object()
+        course_form = CourseForm(instance=course)
+        image_form = ImageForm(instance=course.image)
+        return render(request, self.template_name, {
+            'form': course_form,
+            'image_form': image_form
+        })
+
+    def post(self, request, *args, **kwargs):
+        course = self.get_object()
+        course_form = CourseForm(request.POST, instance=course)
+
+        image_form = ImageForm(
+            request.POST,
+            request.FILES,
+            instance=course.image if course.image else None
+        )
+
+        if course_form.is_valid() and image_form.is_valid():
+            course = course_form.save(commit=False)
+
+            if image_form.cleaned_data.get('image'):
+                image = image_form.save()
+                course.image = image
+
+            course.save()
+            return redirect(self.success_url)
+
+        return render(request, self.template_name, {
+            'form': course_form,
+            'image_form': image_form
+        })
 
 class CourseDeleteView(DeleteView):
     model = Course
