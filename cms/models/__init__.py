0c950c36a00d1bb52997adf7fa6fd24bd15c1a61
umar <amar.m.sheikh@gmail.com>
2025-07-07 12:40:34 +0500

    Add Image model and associate it with Course model

--- cms/models/__init__.py
+++ cms/models/__init__.py
@@ -3,6 +3,7 @@ from .batch_slot import BatchSlot
 from .batch_time import BatchTime
 from .batch import Batch
 from .course import Course
+from .image import Image
 from .instructor_slot import InstructorSlot
 from .instructor import Instructor
 from .parent import Parent
--- cms/models/course.py
+++ cms/models/course.py
@@ -1,6 +1,9 @@
 from django.db import models
+from .image import Image
 
 class Course(models.Model):
+    image = models.OneToOneField(Image, on_delete=models.CASCADE, null=True, blank=True)
+
     title = models.CharField(max_length=255, null= False, blank=False)
     description = models.TextField()
     language = models.CharField(max_length=50)
